954f656b503a41efa73867facb901b1e063593a1
Abir-7 <md.tazwarul.islam.07@gmail.com>
2025-07-21 15:23:34 +0600

    add scroll style

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -6,23 +6,26 @@ import ProtectedRoute from "@/components/ProtectedRoute/ProtectedRoute";
 const layout = ({ children }: { children: ReactNode }) => {
   return (
     <ProtectedRoute>
-      <SidebarProvider className="flex h-screen overflow-hidden">
-        <AppSidebar />
+      <div className="h-full">
+        <SidebarProvider className="flex h-full overflow-hidden">
+          <AppSidebar />
 
-        <main className="w-full">
-          {/* Top bar */}
-          <div className="p-2 bg-gray-900 flex justify-between">
-            <SidebarTrigger className="text-white hover:text-gray-900" />
-            <div className="flex justify-center items-center gap-2">
-              <p className="text-white font-semibold">Hello, Abir</p>
-              <div className="w-10 h-10 bg-white rounded-full"></div>
+          <main className="w-full h-full ">
+            {/* Top bar */}
+            <div className="p-2 bg-gray-900 flex justify-between">
+              <SidebarTrigger className="text-white hover:text-gray-900" />
+              <div className="flex justify-center items-center gap-2">
+                <p className="text-white font-semibold">Hello, Abir</p>
+                <div className="w-10 h-10 bg-white rounded-full"></div>
+              </div>
             </div>
-          </div>
 
-          {/* Scrollable content */}
-          <div className="">{children}</div>
-        </main>
-      </SidebarProvider>
+            <div className="h-[calc(100vh-56px)] overflow-y-auto pb-1 ">
+              {children}
+            </div>
+          </main>
+        </SidebarProvider>
+      </div>
     </ProtectedRoute>
   );
 };
--- src/app/(dashboard)/supervisor/manage-team/page.tsx
+++ src/app/(dashboard)/supervisor/manage-team/page.tsx
@@ -33,6 +33,45 @@ const Page: React.FC = () => {
             <FormInput name="email" label="Email"></FormInput>
             <FormInput name="password" label="Password"></FormInput>
           </BaseForm>
+          <BaseForm
+            isLoading={false}
+            btnText="Add"
+            onSubmit={onSubmit}
+            defaultValues={{ fullName: "", email: "", password: "" }}
+          >
+            <h1 className="font-semibold text-gray-900 ">
+              General Manager Details
+            </h1>
+            <FormInput name="fullName" label="Name"></FormInput>
+            <FormInput name="email" label="Email"></FormInput>
+            <FormInput name="password" label="Password"></FormInput>
+          </BaseForm>
+          <BaseForm
+            isLoading={false}
+            btnText="Add"
+            onSubmit={onSubmit}
+            defaultValues={{ fullName: "", email: "", password: "" }}
+          >
+            <h1 className="font-semibold text-gray-900 ">
+              General Manager Details
+            </h1>
+            <FormInput name="fullName" label="Name"></FormInput>
+            <FormInput name="email" label="Email"></FormInput>
+            <FormInput name="password" label="Password"></FormInput>
+          </BaseForm>
+          <BaseForm
+            isLoading={false}
+            btnText="Add"
+            onSubmit={onSubmit}
+            defaultValues={{ fullName: "", email: "", password: "" }}
+          >
+            <h1 className="font-semibold text-gray-900 ">
+              General Manager Details
+            </h1>
+            <FormInput name="fullName" label="Name"></FormInput>
+            <FormInput name="email" label="Email"></FormInput>
+            <FormInput name="password" label="Password"></FormInput>
+          </BaseForm>
         </TabsContent>
         <TabsContent value="add-team">
           <BaseForm
